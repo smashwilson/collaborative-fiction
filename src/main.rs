38826eab62f92554bc9a948d4884507ddfd149f4
Ashley Wilson <ash.wilson@rackspace.com>
2015-02-01 12:30:02 -0500

    Start with a health check.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+
+fn health_check(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "Up and running.")))
+}
+
 fn main() {
-  println!("Up and running.");
+    Iron::new(health_check).listen("localhost:3000").unwrap();
+
+    println!("Collaborative fiction API server: alive and running.");
 }
