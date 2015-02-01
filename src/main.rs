52a2df4012f648a5305dc1a18c9eddb38c20baed
Ashley Wilson <ash.wilson@rackspace.com>
2015-02-01 12:57:02 -0500

    Use the default logger.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#[macro_use] extern crate log;
+extern crate env_logger;
 extern crate iron;
 extern crate router;
 
@@ -6,15 +8,18 @@ use iron::status;
 use router::Router;
 
 fn health_check(_: &mut Request) -> IronResult<Response> {
+    info!("Health check request.");
+
     Ok(Response::with((status::Ok, "Up and running.")))
 }
 
 fn main() {
+    env_logger::init().unwrap();
+
     let mut router = Router::new();
 
     router.get("/", health_check);
 
+    info!("Launching collaborative fiction API server on localhost:3000.");
     Iron::new(router).listen("localhost:3000").unwrap();
-
-    println!("Collaborative fiction API server: alive and running.");
 }
