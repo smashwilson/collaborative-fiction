ee4337b9107b777fa243bc984ea2e4702fb988f0
Ashley Wilson <ash.wilson@rackspace.com>
2015-02-01 12:35:42 -0500

    Use Iron's router.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 extern crate iron;
+extern crate router;
 
 use iron::prelude::*;
 use iron::status;
+use router::Router;
 
 fn health_check(_: &mut Request) -> IronResult<Response> {
     Ok(Response::with((status::Ok, "Up and running.")))
 }
 
 fn main() {
-    Iron::new(health_check).listen("localhost:3000").unwrap();
+    let mut router = Router::new();
+
+    router.get("/", health_check);
+
+    Iron::new(router).listen("localhost:3000").unwrap();
 
     println!("Collaborative fiction API server: alive and running.");
 }
