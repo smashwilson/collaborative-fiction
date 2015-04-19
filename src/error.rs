cfba69a7f9224141401279b81df68c1acd9fa271
Ash Wilson <smashwilson@gmail.com>
2015-04-19 11:05:35 -0400

    Wrap Hyper and JSON errors too.

--- src/error.rs
+++ src/error.rs
@@ -6,8 +6,11 @@ use std::fmt::{Display, Formatter};
 use std::fmt::Error as FmtError;
 use iron::error::IronError;
 use postgres::Error as PgError;
+use hyper::HttpError;
+use rustc_serialize::json::DecoderError as JSONDecoderError;
+use rustc_serialize::json::EncoderError as JSONEncoderError;
 
-use error::FictError::{Message, Database, Iron};
+use error::FictError::{Message, Database, Iron, Hyper, JSONDecode, JSONEncode};
 
 /// An Error type that can be used throughout the application. It can provide its own error message,
 /// wrap an underlying error, or both.
@@ -16,7 +19,10 @@ use error::FictError::{Message, Database, Iron};
 pub enum FictError {
     Message(String),
     Database(PgError),
-    Iron(IronError)
+    Iron(IronError),
+    Hyper(HttpError),
+    JSONDecode(JSONDecoderError),
+    JSONEncode(JSONEncoderError),
 }
 
 impl Error for FictError {
@@ -25,14 +31,20 @@ impl Error for FictError {
             Message(ref s) => s,
             Database(ref e) => e.description(),
             Iron(ref e) => e.description(),
+            Hyper(ref e) => e.description(),
+            JSONDecode(ref e) => e.description(),
+            JSONEncode(ref e) => e.description(),
         }
     }
 
     fn cause(&self) -> Option<&Error> {
         match *self {
-            Message(_) => None,
+            Message(..) => None,
             Database(ref e) => Some(e),
             Iron(ref e) => Some(e),
+            Hyper(ref e) => Some(e),
+            JSONDecode(ref e) => Some(e),
+            JSONEncode(ref e) => Some(e),
         }
     }
 }
@@ -43,6 +55,9 @@ impl Display for FictError {
             Message(ref s) => f.write_str(s),
             Database(ref e) => Display::fmt(e, f),
             Iron(ref e) => Display::fmt(e, f),
+            Hyper(ref e) => Display::fmt(e, f),
+            JSONDecode(ref e) => Display::fmt(e, f),
+            JSONEncode(ref e) => Display::fmt(e, f),
         }
     }
 }
@@ -56,6 +71,24 @@ impl From<PgError> for FictError {
 impl From<IronError> for FictError {
     fn from(err: IronError) -> FictError {
         FictError::Iron(err)
+    }
+}
+
+impl From<HttpError> for FictError {
+    fn from(err: HttpError) -> FictError {
+        FictError::Hyper(err)
+    }
+}
+
+impl From<JSONDecoderError> for FictError {
+    fn from(err: JSONDecoderError) -> FictError {
+        FictError::JSONDecode(err)
+    }
+}
+
+impl From<JSONEncoderError> for FictError {
+    fn from(err: JSONEncoderError) -> FictError {
+        FictError::JSONEncode(err)
     }
 }
 
